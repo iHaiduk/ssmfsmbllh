71597ec148f57f20924baf80fd2b9e825e937f40
Ihor Haiduk <cookkycok@gmail.com>
2016-04-19 00:32:54 +0300

    i18n programm

---
+++ src/app/app.js
@@ -0,0 +1,12 @@
+/**
+ * Created by igor on 18.04.2016.
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import injectTapEventPlugin from 'react-tap-event-plugin';
+import Main from './Main'; // Our custom react component
+
+injectTapEventPlugin(); 
+
+ReactDOM.render(<Main />, document.getElementById('app'));
